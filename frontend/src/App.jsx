e843900602b3bec7fab40b3c83490d5374be0d3a
vidalgmartin <vidalgmartin@hotmail.com>
2024-02-12 16:41:21 -0500

    Finishing touches

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,4 @@
-// react router to create multiple pages
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 // pages
-import Home from './pages/Home'
 import TaskList from './pages/TaskList'
 
 // components
@@ -13,21 +9,10 @@ export default function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className="pages">
-          <Routes>
-            <Route 
-              path="/"
-              element={<Home />}
-            />
-            <Route 
-              path="/tasks"
-              element={<TaskList />}
-            />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <Navbar />
+      <div className="pages">
+        <TaskList />
+      </div>
     </>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,15 @@
-import { Link } from 'react-router-dom'
-
 export default function Navbar() {
     
     return (
-        <header className="navbar">
-            <div className="navbar-content">
-                <div className="icon">
-                    ICON
-                </div>
-                <div className="links">
-                    <Link to="/">
-                        <h3>Home</h3>
-                    </Link>
-                    <Link to="/tasks">
-                        <h3>Task List</h3>
-                    </Link>
-                </div>
-            </div>
-        </header>
+        <nav className="navbar">
+            <ul className="navbar-content">
+                <li className="name">
+                    Martin
+                </li>
+                <li className="last-name">
+                    <h3>Vidal Garibay</h3>
+                </li>
+            </ul>
+        </nav>
     )
 }

287063536d52ff54c8bef9e77c9e4f4f974e6ddb
vidalgmartin <vidalgmartin@hotmail.com>
2024-02-13 16:08:58 -0500

    proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://mern-todo-app-rbk4.onrender.com',
         changeOrigin: true
       }
     }
